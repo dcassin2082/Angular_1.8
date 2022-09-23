ff86bdd111d29386342ead7e141b8161dc22a268
Daniel Cassin <dcassin2082@gmail.com>
2022-09-23 07:09:29 -0700

    Add files via upload

---
+++ App_Start/BundleConfig.cs
@@ -0,0 +1,45 @@
+﻿using System.Web;
+using System.Web.Optimization;
+
+namespace WebApplication2
+{
+    public class BundleConfig
+    {
+        // For more information on bundling, visit https://go.microsoft.com/fwlink/?LinkId=301862
+        public static void RegisterBundles(BundleCollection bundles)
+        {
+            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
+                        "~/Scripts/jquery-{version}.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                        "~/Scripts/jquery.validate*"));
+
+            // Use the development version of Modernizr to develop with and learn from. Then, when you're
+            // ready for production, use the build tool at https://modernizr.com to pick only the tests you need.
+            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
+                        "~/Scripts/modernizr-*"));
+
+            bundles.Add(new ScriptBundle("~/bundles/angular").Include(
+                 "~/Scripts/angular.js",
+                 "~/Scripts/angular-resource.js",
+                 "~/Scripts/angular-ui/ui-bootstrap.js",
+                 "~/Scripts/angular-ui/ui-bootstrap-tpls.js",
+                 "~/Scripts/angular-ui/ui-bootstrap-io.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/app").Include(
+                     "~/Scripts/app/app.js",
+                     "~/Scripts/app/customer-controller.js",
+                      "~/Scripts/app/customer-factory.js",
+                       "~/Scripts/app/customer-service.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
+                      "~/Scripts/bootstrap.js"));
+
+            bundles.Add(new StyleBundle("~/Content/css").Include(
+                      "~/Content/bootstrap.css",
+                      "~/Content/styles.css",
+                      "~/Content/ui-bootstrap-csp.css",
+                      "~/Content/site.css"));
+        }
+    }
+}
